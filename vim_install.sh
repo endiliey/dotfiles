16b528edc43bbe0189f020aa7363bf05025d0e8e
Endilie Yacop Sucipto <endiliey@gmail.com>
2017-02-12 17:42:21 +0800

    add bash script to install vim

---
+++ vim_install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Remove previous vim
+sudo apt-get remove vim-common vim-runtime
+sudo apt-get remove vim-tiny
+cd ~
+rm -rf vim
+
+# Install vim
+sudo apt-get update
+sudo apt-get install vim vim-nox-py2
+
+#if you want python 3 support, remove vim-nox-py2
+#sudo apt-get install vim
+
+# get vundle
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+touch ~/.vimrc
+
+#copy content of vimrc to .vimrc only (alternatively, can manually copy content):
+#cp ./vimrc ~/.vim/vimrc
+#ln ~/.vimrc ~/.vim/vimrc
+
+#install plugin
+vim +PluginInstall +qall
+
+echo "You need to manually install YoucompleteMe plugin"
+echo "https://github.com/Valloric/YouCompleteMe#ubuntu-linux-x64"
+echo "Enjoy Vim !"
