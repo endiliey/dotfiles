dedc50f4c55820ba497ec2bb6d9109f9e5456fd7
Endilie Yacop Sucipto <endiliey@gmail.com>
2017-09-11 22:37:53 +0800

    Update vim_install.sh

--- vim_install.sh
+++ vim_install.sh
@@ -15,17 +15,16 @@ sudo apt-get install vim vim-nox-py2
 
 # get vundle
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# copy vimrc file in this repo to ~/.vimrc
+sudo cp vimrc ~/.vimrc
 touch ~/.vimrc
 
-#copy content of vimrc to .vimrc only (alternatively, can manually copy content):
-cp ./vimrc ~/.vim/vimrc
-ln ~/.vimrc ~/.vim/vimrc
-cp ./.ycm_extra_conf.py ~/.vim
-
 #install plugin
-vim +PluginInstall +qall
+sudo vim +PluginInstall +qall
 
 echo "You need to manually install YoucompleteMe plugin for python and c-lang family"
 echo "https://github.com/Valloric/YouCompleteMe#ubuntu-linux-x64"
 echo "Don't forget .ycm_extra_conf.py"
+echo "sudo cp .ycm_extra_conf.py ~/.vim"
 echo "Enjoy Vim !"
